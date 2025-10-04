9f6a6b829339c7ba2ab0dfb4bac694f62324fd9b
Diego200803524 <yagi.diego1@hotmail.com>
2025-10-04 01:45:17 +0000

    novo comit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,24 +4,34 @@ int main() {
     int tabuleiro[10][10];
     int i, j;
 
-    // Inicializar o tabuleiro com água (0)
+    // Inicializar tabuleiro com 0 (água)
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // Posicionar navio horizontal (linha 2, colunas 3 a 5)
-    for (j = 3; j < 6; j++) {
-        tabuleiro[2][j] = 3;
+    // Navio horizontal (linha 0, colunas 0 a 2)
+    for (j = 0; j < 3; j++) {
+        tabuleiro[0][j] = 3;
     }
 
-    // Posicionar navio vertical (coluna 7, linhas 5 a 7)
-    for (i = 5; i < 8; i++) {
-        tabuleiro[i][7] = 3;
+    // Navio vertical (coluna 5, linhas 2 a 4)
+    for (i = 2; i < 5; i++) {
+        tabuleiro[i][5] = 3;
     }
 
-    // Exibir o tabuleiro
+    // Navio diagonal principal (posição [6][0] até [8][2])
+    for (i = 0; i < 3; i++) {
+        tabuleiro[6 + i][0 + i] = 3;
+    }
+
+    // Navio diagonal secundária (posição [9][9] até [7][7])
+    for (i = 0; i < 3; i++) {
+        tabuleiro[9 - i][9 - i] = 3;
+    }
+
+    // Exibir tabuleiro
     printf("Tabuleiro de Batalha Naval (0 = agua, 3 = navio):\n\n");
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
